a27607430490a8f7f511ce30916b753c304c9c3e
duwon1229 <44364759+duwon1229@users.noreply.github.com>
2023-02-02 22:50:08 +0900

    localstorage 조회 기능 구현

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <label for="todo-input"></label>
+    <label for="todo-input">오늘 할 일 : </label>
     <input id="todo-input" type="text" :value="item" @input="handleInput" />
     <button @click="addTodo">add</button>
   </div>
@@ -10,14 +10,20 @@
 import Vue from "vue";
 
 export default Vue.extend({
-  props: ["item"],
+  //   props: ["item"],
+  props: {
+    item: {
+      type: String,
+      required: true,
+    },
+  },
   methods: {
-    handleInput(event: any) {
-      event.target.value;
-      this.$emit("input", event.target.value);
+    handleInput(event: InputEvent) {
+      const eventTarget = event.target as HTMLInputElement;
+      this.$emit("input", eventTarget.value);
     },
     addTodo() {
-      console.log("add");
+      this.$emit("add");
     },
   },
 });
