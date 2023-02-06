8e7e4e35194292bc134e0f4b4f2e31757f886ff0
duwon1229 <44364759+duwon1229@users.noreply.github.com>
2023-02-06 22:11:29 +0900

    할일 list 수정

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,7 @@
             :key="index"
             :index="index"
             :todoItem="todoItem"
+            @toggle="toggleTodoItemComplete"
             @remove="removeTodoItem"
           />
           <!-- <li>아이템 1</li>
@@ -41,12 +42,17 @@ const storage = {
   },
 };
 
+export interface Todo {
+  title: string;
+  done: boolean;
+}
+
 export default Vue.extend({
   components: { TodoInput, TodoListItem },
   data() {
     return {
       todoText: "",
-      todoItems: [] as any,
+      todoItems: [] as Todo[],
     };
   },
   methods: {
@@ -55,7 +61,11 @@ export default Vue.extend({
     },
     addTodoItem() {
       const value = this.todoText;
-      this.todoItems.push(value);
+      const todo: Todo = {
+        title: value,
+        done: false,
+      };
+      this.todoItems.push(todo);
       storage.save(this.todoItems);
       this.intiTodoText();
     },
@@ -68,6 +78,13 @@ export default Vue.extend({
     removeTodoItem(index: number) {
       console.log("remove", index);
       this.todoItems.splice(index, 1);
+      storage.save(this.todoItems);
+    },
+    toggleTodoItemComplete(todoItem: Todo, index: number) {
+      this.todoItems.splice(index, 1, {
+        ...todoItem,
+        done: !todoItem.done,
+      });
       storage.save(this.todoItems);
     },
   },
